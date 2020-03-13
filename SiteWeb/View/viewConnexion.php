113dd648bcde4072bce1f7ab94bd8581a636a6c7
Loucaluc <loucaluc@live.fr>
2020-03-13 15:45:33 -0400

    Page connexion amélioré
    - Ajout de gros textbox + boutton
    - Ajout du logo mecanium

--- SiteWeb/View/viewConnexion.php
+++ SiteWeb/View/viewConnexion.php
@@ -2,24 +2,29 @@
 
 <?php ob_start(); ?>
 
-<div class="">
-    
+<div class="container text-center">
+    <div class="">
+        <a href="">
+            <img src="Assets/Images/Mecanium.png" alt="Mecanium Logo" class="img-responsive center-block">
+        </a>
+    </div>
 
     <div class="contentSpacing">
         <form id="frm_connexion" name="frm_connexion" method="POST" action="#">
 
             <div class="form-group">
-                <label for="txt_username">Username</label>
-                <input type="text" class="form-control" id="txt_username" name="txt_username" placeholder="Username">
+                <input type="text" class="form-control form-control-lg mb-2" id="txt_username" name="txt_username" placeholder="Nom d'utilisateur">
             </div>
 
             <div class="form-group">
-                <label for="txt_mdp">Password</label>
-                <input type="password" class="form-control" id="txt_mdp" name="txt_mdp" placeholder="Password">
+                <input type="password" class="form-control form-control-lg mb-2" id="txt_mdp" name="txt_mdp" placeholder="Mot de passe">
             </div>
 
             <div id="etat_connexion" style="color:red"></div>
-        <button type="submit" class="btn btn-primary">Login</button>
+            
+            <div class="">
+                <button type="submit" class="btn btn-primary btn-lg">Login</button>
+            </div>
     </form>
 </div> 
 </div> 
