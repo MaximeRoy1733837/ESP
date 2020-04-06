d45104d6ddb2a84deb3886f8a56b318097a7f854
Loucaluc <loucaluc@live.fr>
2020-04-06 10:29:54 -0400

    Mod. menu + début page accueil
    - Changement couleur menu et nom des onglets
    -Ajout de card pour les quantités bon et mauvais

--- SiteWeb/View/viewMenu.php
+++ SiteWeb/View/viewMenu.php
@@ -1,14 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar navbar-dark fixed-top bg-dark">
   <a class="navbar-brand" href="#">Siemens S7-1200</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+      <a class="nav-item nav-link <?php if($currentPage == "Home"){?>  active <?php } ?>" href="index.php?action=Home">Home</a>
+      <a class="nav-item nav-link <?php if($currentPage == "Historique"){?>  active <?php } ?>" href="index.php?action=Historique">Historique</a>
+      <a class="nav-item nav-link" href="#">Se déconnecter</a>
     </div>
   </div>
 </nav>
--- SiteWeb/index.php
+++ SiteWeb/index.php
@@ -12,6 +12,7 @@
 	}	
 	else {
 		//Connexion(); 
-		Historique();
+		//Historique();
+		Homepage();
 	}
 ?>
