8372d4375d6d0771fbf5f80ea36ac65d9f551597
Loucaluc <loucaluc@live.fr>
2020-04-07 10:03:10 -0400

    update de la progressBar

--- SiteWeb/Assets/JS/gestion_accueil.js
+++ SiteWeb/Assets/JS/gestion_accueil.js
@@ -8,6 +8,8 @@
             url: "ajaxhandler.php?event=GetNewInfo",
             success: function(output) {
                 var data = JSON.parse(output);
+                var percent = 0;
+
                 $('#nom_commande').html(data["nom_commande"]);
                 $('#quantite_produire').html(data["quantite_produire"]);
                 $('#quantite_bon').html(data["quantite_bon"]);
@@ -15,6 +17,14 @@
                 $('#temperature').html(data["temperature"]);
                 $('#humidite').html(data["humidite"]);
                 $('.lastUpdateDate').html('Dernière mise à jour: ' + data["date"]);
+
+                if (parseInt(data["quantite_produire"]) > 0)
+                {
+                  percent = Math.round((parseInt(data["quantite_bon"]) / parseInt(data["quantite_produire"]) * 100));
+                }         
+
+                $('#progression').html(percent + '%');
+                $('#progression').attr('aria-valuenow', percent).css('width', percent + '%');
           }
         });
     },3000);
--- SiteWeb/View/viewHome.php
+++ SiteWeb/View/viewHome.php
@@ -84,7 +84,7 @@
     <br>
 
     <div class="progress">
-        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+        <div id="progression" class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 
     <br>
