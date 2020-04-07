c5977cdd8bf42d14477542ef69e0e9dcfd852335
MaximeRoy1733837 <maxroy14@hotmail.ca>
2020-04-07 16:15:10 -0400

    continuer script python

--- Python/mqtt_Pub.py
+++ Python/mqtt_Pub.py
@@ -1,16 +1,38 @@
 import time
 import datetime
 import paho.mqtt.publish as publish
+import random
+import sys
+
+temperature = 24
+humidite = 20
+commande = input("Entrez le nom de la commande : ")
+quantite = input("Entrez la quantite de bouchon a produire : ")
+quantiteBon = 0
+quantiteMauvais = 0
 
 try:
     while True:
 
+        temperature = random.randint(18, 25)
+        humidite = random.randint(18, 25)
+        quantiteBon = quantiteBon + 10
+        quantiteMauvais = quantiteMauvais + 2
+
         publish.single("Mecanium/ESP/Temps", str(datetime.datetime.now()), hostname="test.mosquitto.org")
-        publish.single("Mecanium/ESP/Temperature", 24, hostname="test.mosquitto.org")
-        publish.single("Mecanium/ESP/Humidite", 18, hostname="test.mosquitto.org")
+        publish.single("Mecanium/ESP/Temperature", temperature, hostname="test.mosquitto.org")
+        publish.single("Mecanium/ESP/Humidite", humidite, hostname="test.mosquitto.org")
+        publish.single("Mecanium/ESP/Commande", commande, hostname="test.mosquitto.org")
+        publish.single("Mecanium/ESP/Quantite", quantite, hostname="test.mosquitto.org")
+        publish.single("Mecanium/ESP/Quantite_bon", quantiteBon, hostname="test.mosquitto.org")
+        publish.single("Mecanium/ESP/Quantite_mauvais", quantiteMauvais, hostname="test.mosquitto.org")
         print("Done")
 
         time.sleep(6)
+
+        if quantiteBon >= int(quantite):
+            print("Terminer")
+            sys.exit()
 
 except KeyboardInterrupt:
     print("Cleanup")
