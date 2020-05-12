d66ca8b45f3c6eb13cd56412f7c143e6e98c875b
MaximeRoy1733837 <maxroy14@hotmail.ca>
2020-05-12 09:54:20 -0400

    modifier guide et commencer rapport

--- SiteWeb/Model/connexion.php
+++ SiteWeb/Model/connexion.php
@@ -3,6 +3,7 @@ class Connexion
 {
     protected function getConnexion()
     {
+        //Changer le mot de passe
         $connexion = new \PDO('mysql:host=localhost;dbname=bd_esp;charset=utf8', 'root', '',array( 
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8" ,
